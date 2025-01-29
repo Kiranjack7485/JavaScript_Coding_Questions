7280f7c0ca465391e0e846b0d552ac37d9787ff1
Kiranjack7485 <106486012+Kiranjack7485@users.noreply.github.com>
2025-01-29 19:53:13 +0530

    Update LargestNumByLooping.js

--- LargestNumByLooping.js
+++ LargestNumByLooping.js
@@ -1,7 +1,7 @@
 myArr = [106,39,18,45,32,111,110,37];
 
 function maxFuncByLoop (myArr){
-    let maxNum = myArr[0];
+    let maxNum = 0;
     for (let i=0;i<myArr.length;i++){
         if(myArr[i]>maxNum){
             maxNum = myArr[i]
