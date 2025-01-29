99405dd6737bf60e8bc22f7f9b6489b2642ba887
Kiranjack7485 <106486012+Kiranjack7485@users.noreply.github.com>
2025-01-29 20:46:33 +0530

    Update LargestNumByMathmax.js

--- LargestNumByMathmax.js
+++ LargestNumByMathmax.js
@@ -1,12 +1,5 @@
-function largestNum (numArr) {
-    let largeNum = numArr[0]
-    for (let i =0;i<numArr.length;i++){
-        if (numArr[i]>largeNum){
-            largeNum = numArr[i]
-            return largeNum
-        }
-        
-    }
+function largestNum (myArr){
+    return Math.max(... myArr);
 }
 
-console.log(largestNum([100,102,99,34,15]))
+console.log(largestNum([10,2,3,12,15]));
